f4259d7b0e9e2fa2d3091ff4c35282d02414e5f0
wasdfg <leejh9711@naver.com>
2024-01-06 17:14:23 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 3.67 ms, Memory: 38.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int solution(vector<vector<int>> board){
+    int answer = board[0][0];
+    for(int i = 1;i < board.size();i++){
+        for(int j = 1;j < board[i].size();j++){
+            if(board[i][j] == 1){
+                board[i][j] = 1 + min({board[i-1][j-1],board[i-1][j],board[i][j-1]});
+                answer = max(answer,board[i][j]);
+            }
+        }
+    }
+    return answer*answer;
+}
