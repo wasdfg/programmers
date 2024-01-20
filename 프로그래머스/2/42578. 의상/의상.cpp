7b77c4b51234d6bdca0d79d60da7845c989856bc
wasdfg <leejh9711@naver.com>
2024-01-20 15:51:32 +0900

    Update 의상.cpp

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -7,17 +7,17 @@ using namespace std;
 int solution(vector<vector<string>> clothes) {
     int answer = 1;
     map<string,int> wear;
-    for(int i = 0;i < clothes.size();i++){
-        if(wear[clothes[i][1]] == 0){
+    for(int i = 0;i < clothes.size();i++){ //map을 사용해서 옷 종류를 집어넣음
+        if(wear[clothes[i][1]] == 0){ //같은 옷 종류의 수를 구함
             wear[clothes[i][1]] = 1;
         }
         else{
             wear[clothes[i][1]]++;
         }
     }
-    for(auto iter : wear){
+    for(auto iter : wear){ //옷을 입는다 안입는다 2가지 방법이 있으므로 조합을 사용한다
 	    answer *= iter.second+1;
     }
-    answer--;
+    answer--; //전부 안입는것은 제외
     return answer;
 }
